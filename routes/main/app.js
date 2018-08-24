7a823a54911b76235cc4c163eb48e7319994f92c
Samyak Jain <hackmykaamprivate@gmail.com>
2018-08-24 03:21:56 -0700

    Delete app.js

--- routes/main/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('app', { title: 'GirlScript_India'});
-});
-
-module.exports = router;
